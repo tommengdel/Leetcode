ca9bab7d5f31216f16015929b042f65b4963bd15
tommengdel <tommengdel@gmail.com>
2013-08-18 16:47:05 +0800

    ZigZag

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -5,14 +5,17 @@ public:
         // DO NOT write int main() function    
         string ret;
         ret.resize(s.size());
+        int count = 0;
+        if(nRows == 1) return s; //∫‹÷ÿ“™
         int cirnum = 2 * nRows - 2;
-        for(int i = 0; i < s.size(); i += cirnum) ret.push_back(s[i]);
+        for(int i = 0; i < s.size(); i += cirnum) ret[count++] = s[i];
+	//Can't use ret.push_back, or the memeory will become 2 * s.size()
         for(int i = 1; i < nRows - 1; ++i)
             for(int j = i; j < s.size(); j += cirnum) {
-                ret.push_back(s[j]);
-                if(j + cirnum - 2 * i < s.size()) ret.push_back(s[j + cirnum - 2 * i]);
+                ret[count++] = s[j];
+                if(j + cirnum - 2 * i < s.size()) ret[count++] = s[j + cirnum - 2 * i];
             }
-        for(int i  = nRows - 1; i < s.size(); i += cirnum) ret.push_back(s[i]);
+        for(int i  = nRows - 1; i < s.size(); i += cirnum) ret[count++] = s[i];
         return ret;
     }
 };
