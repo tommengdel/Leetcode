aa73512baaed208287da6eb5bab27ee6eda4972c
tommengdel <tommengdel@gmail.com>
2013-08-18 16:45:39 +0800

    ZigZag

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -1,29 +1,19 @@
-#include<iostream>
-#include<string>
-using namespace std;
 class Solution {
 public:
     string convert(string s, int nRows) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function    
-        
-        //First row
-        string ret = "";
+        string ret;
         ret.resize(s.size());
-        int count = 0;
-        //first row
-        for(int i = 0; i < s.size(); i += 2 * nRows - 2) ret[count++] = s[i];
-        
-        //Intermediate rows
-        for(int  i = 1;  i < nRows - 1; ++i) {
-            for(int j = 1; j < s.size(); j += 2 * nRows - 2) {
-                ret[count++] = s[j];
-                if(j + 2 * nRows - 4 < s.size()) ret[count++] = s[j + 2 * nRows - 4];
+        int cirnum = 2 * nRows - 2;
+        for(int i = 0; i < s.size(); i += cirnum) ret.push_back(s[i]);
+        for(int i = 1; i < nRows - 1; ++i)
+            for(int j = i; j < s.size(); j += cirnum) {
+                ret.push_back(s[j]);
+                if(j + cirnum - 2 * i < s.size()) ret.push_back(s[j + cirnum - 2 * i]);
             }
-        }
-        //Last row
-        for(int i = nRows - 1; i < s.size(); i += 2 * nRows - 2) ret[count++] = s[i];
+        for(int i  = nRows - 1; i < s.size(); i += cirnum) ret.push_back(s[i]);
         return ret;
-    }   
-    
+    }
 };
+
