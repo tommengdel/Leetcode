a00935530506bad32ffdc8cbebf08a41fb8a19b6
tommengdel <tommengdel@gmail.com>
2013-09-16 12:57:58 +0800

    SimplePath Revised

--- SimplePath.cpp
+++ SimplePath.cpp
@@ -10,7 +10,7 @@ public:
         while(ind < path.size() && path[ind] == '/') ++ind;
         while(ind < path.size()) {
             int j = ind + 1;
-            while(path[j] != '/') ++j;
+            while(j < path.size() && path[j] != '/') ++j;
             string curPath = path.substr(ind, j - ind);
             if(curPath == "..") {
                 if(pathes.size() != 1) pathes.pop_back();
@@ -22,11 +22,14 @@ public:
         }
         
         string simplePath = pathes[0];
+        if(pathes.size() == 1) return simplePath;
+        
         for(int i = 1; i < pathes.size(); ++i) {
             simplePath += pathes[i];
             simplePath += '/';
         }
         simplePath.pop_back();
+        
         return simplePath;
     }
 };
