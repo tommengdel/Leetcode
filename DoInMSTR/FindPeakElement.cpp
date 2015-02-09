1f7c3aa8dbe12c6312ba87165fbfd4e036053358
huhuang <huhuang@microstrategy.com>
2015-02-09 06:12:51 -0800

    remove useless comparison in FindPeakElement.cpp

--- DoInMSTR/FindPeakElement.cpp
+++ DoInMSTR/FindPeakElement.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int findPeakElement(const vector<int> &num) {
           if(num.size() == 0) return -1;
-          if(num.size() == 1) return 0;
-          if(num[0] > num[1]) return 0;
-          if(num[num.size() - 1] > num[num.size() - 2]) return num.size() - 1;
-          
-          for(int i = 1; i < num.size() - 1; ++i)
-             if(num[i] > num[i -1] && num[i] > num[i + 1]) return i;
+          int peak = 0; 
+          while(peak < num.size() - 1) {
+              if(num[peak] > num[peak + 1]) break;
+              else ++peak;
+          }
+          return peak;
     }
 };
-//Code Is too ugly.
-//You need to improve it.
+//Remove useless comparison
