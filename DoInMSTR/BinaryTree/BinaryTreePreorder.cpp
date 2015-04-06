32c0b20faeac429806c7929ff812728c56a7a050
unknown <tommengdel@gmail.com>
2015-04-06 21:56:05 +0800

    Simplify preorder

--- DoInMSTR/BinaryTree/BinaryTreePreorder.cpp
+++ DoInMSTR/BinaryTree/BinaryTreePreorder.cpp
@@ -1,35 +1,28 @@
 /**
  * Definition for binary tree
-  * struct TreeNode {
-   *     int val;
-    *     TreeNode *left;
-     *     TreeNode *right;
-      *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-       * };
-        */
-	class Solution {
-	public:
-	    vector<int> preorderTraversal(TreeNode *root) {
-	            stack<TreeNode* > st;
-		            vector<int> ret(0);
-			            
-				            //Preprocess
-					            TreeNode *cur = root;
-						            while(cur != 0) {
-							                ret.push_back(cur->val);
-									            if(cur->right != 0) st.push(cur->right);
-										                cur = cur->left;
-												        }
-													        
-														        while(!st.empty()) {
-															            cur = st.top();
-																                st.pop();
-																		            while(cur != 0) {
-																			                    ret.push_back(cur->val);
-																					                    if(cur->right != 0) st.push(cur->right);
-																							                    cur = cur->left;
-																									                }
-																											        }
-																												        return ret;
-																													    }
-																													    };
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        stack<TreeNode* > st;
+        vector<int> ret(0);
+    
+        st.push(root);
+        while(!st.empty()) {
+            TreeNode* cur = st.top();
+            st.pop();
+            while(cur != 0) {
+                ret.push_back(cur->val);
+                if(cur->right != 0) st.push(cur->right);
+                cur = cur->left;
+            }
+        }
+        return ret;
+    }
+};
